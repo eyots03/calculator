387823b0d223cdc4e9883d8da97b3911ca585cda
eyoo <eyots04@gmail.com>
2026-03-25 16:54:11 +0300

    Equal work

--- script.js
+++ script.js
@@ -8,8 +8,7 @@ let backSpace = document.querySelector("#back");
 let operationUse = false; // tells if an operation is in use
 let dispNum = []; // used for storing number after operation
 let dispOp = []; // used for storing the operation
-let operator;
-let dispNum2 = "";
+let operator; // chooses the right operator from disOp
 let total = 0;
 
 function operationDefault() {
@@ -19,16 +18,51 @@ function operationDefault() {
             })
 }
 
-function equals() {
-    equal.addEventListener("click", () => {
-
-    })
-}
-
 function operationStatus(button) {
     button.style.backgroundColor = "black";
     button.style.color = "white";
     operationUse = true;
+}
+
+function operationCalc(preValue) {
+    switch(operator) {
+        case "+":
+            if(total === 0) {
+                display.textContent = preValue + Number(display.textContent);
+                total = Number(display.textContent);
+            } else {
+                display.textContent = total + Number(display.textContent);
+                total = Number(display.textContent);
+            }
+            break;
+        case "-":
+            if(total === 0) {
+                display.textContent = preValue - Number(display.textContent);
+                total = Number(display.textContent);
+            } else {
+                display.textContent = total - Number(display.textContent);
+                total = Number(display.textContent);
+            }
+            break;
+        case "×":
+            if(total === 0) {
+                display.textContent = preValue * Number(display.textContent);
+                total = Number(display.textContent);
+            } else {
+                display.textContent = total * Number(display.textContent);
+                total = Number(display.textContent);
+            }
+            break;
+        case "÷":
+            if(total === 0) {
+                display.textContent = preValue / Number(display.textContent);
+                total = Number(display.textContent);
+            } else {
+                display.textContent = total / Number(display.textContent);
+                total = Number(display.textContent);
+            }
+            break;            
+    }
 }
 
 num.forEach(button => { // function for when a number is clicked
@@ -38,7 +72,6 @@ num.forEach(button => { // function for when a number is clicked
             operationUse = false;
             display.textContent += button.textContent;
             operationDefault();
-            equals();
         } else {
                 display.textContent += button.textContent;
             }
@@ -50,85 +83,32 @@ operation.forEach(button => { // function for when an operation is clicked
     button.addEventListener("click", () => {
         dispNum.push(Number(display.textContent));
         dispOp.push(button.textContent);
+
         if(dispNum.length < 2) {
             operationDefault();
             operationStatus(button);
-            // if(total !== 0) {
-            //     operator = dispOp[dispOp.length - 2];
-            //     switch(operator) {
-            //         case "+":
-            //             display.textContent = dispNum + total;
-            //             total += dispNum;
-            //             break;
-            //         case "-":
-            //             display.textContent = total - dispNum;
-            //             total -= dispNum;   
-            //             break;
-            //         case "×":
-            //             display.textContent = total * dispNum;
-            //             total *= dispNum;
-            //             break;
-            //         case "÷":
-            //             display.textContent = total / dispNum;
-            //             total /= dispNum;
-            //     }
-            // }
-
         } else {
-            // dispNum2 = Number(display.textContent);
-            // dispOp.length > 2 ? operator = dispOp[dispOp.length - 2] : 
             operator = dispOp[dispOp.length - 2];
-            switch(operator) {
-                case "+":
-                    if(dispNum.length === 2) {
-                        display.textContent = dispNum[dispNum.length - 2] + dispNum[dispNum.length - 1];
-                        total = Number(display.textContent);
-                    } else {
-                        display.textContent = total + dispNum[dispNum.length - 1];
-                        total = Number(display.textContent);
-                    }
-                    break;
-                case "-":
-                    if(dispNum.length === 2) {
-                        display.textContent = dispNum[dispNum.length - 2] - dispNum[dispNum.length - 1];
-                        total = Number(display.textContent);
-                    } else {
-                        display.textContent = total - dispNum[dispNum.length - 1];
-                        total = Number(display.textContent);
-                    }
-                    break;
-                case "×":
-                    if(dispNum.length === 2) {
-                        display.textContent = dispNum[dispNum.length - 2] * dispNum[dispNum.length - 1];
-                        total = Number(display.textContent);
-                    } else {
-                        display.textContent = total * dispNum[dispNum.length - 1];
-                        total = Number(display.textContent);
-                    }
-                    break;
-                case "÷":
-                    if(dispNum.length === 2) {
-                        display.textContent = dispNum[dispNum.length - 2] / dispNum[dispNum.length - 1];
-                        total = Number(display.textContent);
-                    } else {
-                        display.textContent = total / dispNum[dispNum.length - 1];
-                        total = Number(display.textContent);
-                    }
-                    break;            
-            }
+            operationCalc(dispNum[dispNum.length - 2]);
             operationDefault();
             operationStatus(button);
         }
     })
+})
+
+equal.addEventListener("click", () => {
+    operator = dispOp[dispOp.length - 1];
+    operationCalc(dispNum[dispNum.length - 1]);
+    console.log(total);
 })
              
 clear.addEventListener("click", () => {
     display.textContent = "";
     dispNum.length = 0;
     dispOp.length = 0;
-    dispNum2 = "";
     total = 0;
     operationDefault();
-});
-backSpace.addEventListener("click", () => display.textContent = display.textContent.slice(0, -1));
+})
+
+backSpace.addEventListener("click", () => display.textContent = display.textContent.slice(0, -1))
 
